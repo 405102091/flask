2eb3577d026b99da3a67768a7e1a82a224ceb000
root <root@localhost.localdomain>
2018-03-27 23:50:48 +0800

    get&post

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template,send_from_directory
+from flask import request,render_template,send_from_directory
 
 @app.route('/')
 def index():
@@ -8,3 +8,10 @@ def index():
 @app.route('/file/<path:path>')
 def send_file(path):
     return send_from_directory('./file',path)
+
+@app.route('/api/request',methods={'POST','GET'})
+def api_get():
+    # request.form.get('key',type=str,default=None) # get form data
+    # request.args.get('key') # get http-get data
+    # request.value.get('key') # get all the pramaters include get and post
+    return str(request.values)
