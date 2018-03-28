f11a3f29b5aee4e0f811ef8043af4edfa3be6e9a
root <root@localhost.localdomain>
2018-03-28 22:15:04 +0800

    eleadmin

--- app/views.py
+++ app/views.py
@@ -1,13 +1,30 @@
 from app import app
 from flask import request,render_template,send_from_directory
 
-@app.route('/')
-def index():
-    return 'hello world!'
+@app.route('/<path:path>')
+def index(path):
+    return render_template(path) # auto search in templates folder
 
 @app.route('/file/<path:path>')
 def send_file(path):
     return send_from_directory('./file',path)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('./static/js',path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('./static/css',path)
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('./static/images',path)
+
+@app.route('/icons/<path:path>')
+def send_icons(path):
+    return send_from_directory('./static/icons',path)
+
 
 @app.route('/api/request',methods={'POST','GET'})
 def api_request():
