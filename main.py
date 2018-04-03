c691fdd364b53a853607464d260ff8a7375eadcc
root <root@localhost.localdomain>
2018-04-03 19:42:56 +0800

    o.o

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,use_debugger=False,threaded=True)
+    app.run()#(host='0.0.0.0',port=80,use_debugger=False,threaded=True)
